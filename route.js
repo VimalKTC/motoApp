36294ab7c4fc6c0c1ac8c2f04ccd3fb2ccdcf0c9
MotoClique123 <motoclique123@gmail.com>
2018-07-03 01:36:34 +0530

    test
    post test

--- route.js
+++ route.js
@@ -9,6 +9,7 @@ var auth = jwt({
 // authentication
 //require('./models/users');
 var ctrlAuth = require('./controllers/authentication');
+router.post('/test', function(req,res){res.json({results: req.body});});
 router.post('/register', ctrlAuth.register);
 router.post('/login', ctrlAuth.login);
 
