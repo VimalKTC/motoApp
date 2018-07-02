6b4b2e80ae0320f69e17fcc623ed3fa8932c16b3
MotoClique123 <motoclique123@gmail.com>
2018-07-02 23:53:17 +0530

    Complete Upload
    Complete upload

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
-//  OpenShift sample Node application
+//  Moto Clique Node application
 //importing modules
 
-
-
-//  OpenShift sample Node application
 var express = require('express'),
 app     = express(),
 morgan  = require('morgan');
@@ -11,7 +8,57 @@ morgan  = require('morgan');
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+app.use(morgan('combined'));
+
+
+// BRING IN YOUR SCHEMAS & MODELS
+//admin schemas
+require('./models/users');
+require('./models/profile');
+require('./models/subscription');
+require('./models/spec_fields');
+require('./models/screens');
+require('./models/role');
+require('./models/product_type');
+require('./models/product_thumbnail');
+require('./models/product_spec');
+require('./models/product_image');
+require('./models/product_hierarchy');
+require('./models/product');
+require('./models/prd_typ_spec_field_map');
+require('./models/otp');
+require('./models/fields');
+require('./models/brand');
+require('./models/application');
+require('./models/app_scr_fields_rights');
+require('./models/country_state_city_loc');
+require('./models/parameter');
+
+//endusers schemas
+require('./models/enduser/bid');
+require('./models/enduser/bid_by');
+require('./models/enduser/buy_request');
+require('./models/enduser/favourite');
+require('./models/enduser/filter');
+require('./models/enduser/images');
+require('./models/enduser/sell');
+require('./models/enduser/thumbnails');
+require('./models/enduser/user_address');
+require('./models/enduser/user_alert');
+require('./models/enduser/user_sub_map');
+require('./models/enduser/service');
+require('./models/enduser/feedback');
+require('./models/enduser/rating');
+require('./models/enduser/thumbs_up');
+require('./models/enduser/thumbs_down');
+require('./models/enduser/counter');
+
+var passport = require('passport');
+const route = require('./route');
+require('./config/passport');
+
+app.use(passport.initialize());
+app.use('/api',route);
 
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
